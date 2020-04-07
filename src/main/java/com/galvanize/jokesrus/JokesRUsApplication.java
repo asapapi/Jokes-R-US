3efd4a3bebf59b73695bf55a3ef9943ede881ca3
Asahi <asahimojica@yahoo.com>
2020-04-07 18:55:14 -0500

    jokes setup

---
+++ src/main/java/com/galvanize/jokesrus/JokesRUsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.jokesrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesRUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesRUsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/jokesrus/JokesRUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.jokesrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesRUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
